dc4b6a1c4f5fd6fe06972c952c83338269daf7c8
oleksandrdrohan <113466016+oleksandrdrohan@users.noreply.github.com>
2023-07-19 12:00:52 +0300

    Add files via upload

---
+++ ChatServer/src/main/java/academy/prog/GetListServlet.java
@@ -0,0 +1,41 @@
+package academy.prog;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+import jakarta.servlet.http.*;
+
+public class GetListServlet extends HttpServlet {
+
+	private MessageList msgList = MessageList.getInstance();
+
+	private UsersList onlineUsers = new UsersList();
+
+	private String receiver;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String fromStr = req.getParameter("from");
+		receiver = req.getParameter("receiver");
+		int from = 0;
+		try {
+			from = Integer.parseInt(fromStr);
+			if (from < 0) from = 0;
+		} catch (Exception ex) {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+
+		onlineUsers.getRequestFromUsers(receiver);
+
+		resp.setContentType("application/json");
+
+		String json = msgList.toJSON(from,receiver);
+		if (json != null) {
+			OutputStream os = resp.getOutputStream();
+			byte[] buf = json.getBytes(StandardCharsets.UTF_8);
+			os.write(buf);
+		}
+	}
+}
---
+++ ChatServer/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Prog Academy chat server</title>
+</head>
+<body>
+<h1>Server works!</h1>
+<br>
+<h2><a href="http://localhost:8080/users-online">Users online</a></h2>
+</body>
+</html>
